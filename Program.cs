c796fdd3a49ef445afafba853036a49e90aa40e8
GuilhermeLCS <guilhermeluiscdasilva@hotmail.com>
2023-02-01 01:51:35 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SalesWebMvc.Data;
+using System.Configuration;
+using SalesWebMvc.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+namespace SalesWebMvc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);       
+            builder.Services.AddDbContext<SalesWebMvcContext>(options =>
+            options.UseSqlServer("Data Source=BEATRICE;Initial Catalog=saleswebmvcappdb;Integrated Security=False;User ID=guilherme;Password=12345;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False"));
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddScoped<SeedingService>();
+            builder.Services.AddScoped<SellerService>();
+            builder.Services.AddScoped<DepartmentService>();
+            var app = builder.Build();
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
